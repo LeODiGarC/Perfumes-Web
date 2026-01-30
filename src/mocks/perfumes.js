2e6d8146bea7eff5460a33c32ca8e07c9babe240
LeODiGarC <leonardodg2000@gmail.com>
2026-01-30 12:58:18 -0500

    updated list

--- src/mocks/perfumes.js
+++ src/mocks/perfumes.js
@@ -1,178 +1,297 @@
-import CeriseRouge from '../assets/CeriseRouge.jpg';
-import CrystalAngel from '../assets/CrystalAngel.jpg';
-import DalaAlBanat from '../assets/DalaAlBanat.jpg';
-import EmperBromusk from '../assets/EmperBromusk.jpg';
-import EmperHighFashionNiche053 from '../assets/EmperHighFashionNiche053.jpg';
-import EmperHighFashionNiche057 from '../assets/EmperHighFashionNiche057.jpg';
-import EmperHighFashionNiche058 from '../assets/EmperHighFashionNiche058.jpg';
-import EmperMiramar from '../assets/EmperMiramar.webp';
-import EmperPureCollectionNiche059 from '../assets/EmperPureCollectionNiche059.webp';
-import FifthAvenueOfNewYork from '../assets/FifthAvenueOfNewYork.webp';
-import Melina from '../assets/Melina.jpg';
-import MilestoneMadamMoiselle from '../assets/MilestoneMadamMoiselle.jpeg';
-import MilestoneMonarchCandyBlast from '../assets/MilestoneMonarchCandyBlast.jpg';
-import MilestoneRoseBlanc from '../assets/MilestoneRoseBlanc.jpeg';
-import PaintingsCollectionLordGordon from '../assets/PaintingsCollectionLordGordon.jpeg';
-import PrivéBordeux from '../assets/PrivéBordeux.jpeg';
-import PrivéIllusion from '../assets/PrivéIllusion.jpg';
-import PrivéIllusionIntense from '../assets/PrivéIllusionIntense.jpg';
-import PrivéModaIsMyIdole from '../assets/PrivéModaIsMyIdole.webp';
-import PrivéPinkRose from '../assets/PrivéPinkRose.jpg';
-import Queen from '../assets/Queen.webp';
-import SenoBeauty from '../assets/SenoBeauty.webp';
-import SerAlAmeera from '../assets/SerAlAmeera.webp';
-import SoPure from '../assets/SoPure.jpg';
-import ValenciaViceVersa from '../assets/ValenciaViceVersa.jpg';
+import CeriseRouge from "../assets/CeriseRouge.jpg";
+import CrystalAngel from "../assets/CrystalAngel.jpg";
+import DalaAlBanat from "../assets/DalaAlBanat.jpg";
+import EmperBromusk from "../assets/EmperBromusk.jpg";
+import EmperHighFashionNiche053 from "../assets/EmperHighFashionNiche053.jpg";
+import EmperHighFashionNiche057 from "../assets/EmperHighFashionNiche057.jpg";
+import EmperHighFashionNiche058 from "../assets/EmperHighFashionNiche058.jpg";
+import EmperMiramar from "../assets/EmperMiramar.webp";
+import EmperPureCollectionNiche059 from "../assets/EmperPureCollectionNiche059.webp";
+import FifthAvenueOfNewYork from "../assets/FifthAvenueOfNewYork.webp";
+import Melina from "../assets/Melina.jpg";
+import MilestoneMadamMoiselle from "../assets/MilestoneMadamMoiselle.jpeg";
+import MilestoneMonarchCandyBlast from "../assets/MilestoneMonarchCandyBlast.jpg";
+import MilestoneRoseBlanc from "../assets/MilestoneRoseBlanc.jpeg";
+import PaintingsCollectionLordGordon from "../assets/PaintingsCollectionLordGordon.jpeg";
+import PaintigsCollectionPaonne from "../assets/PaintingsCollectionPaonne.avif";
+import PrivéBordeux from "../assets/PrivéBordeux.jpeg";
+import PrivéIllusion from "../assets/PrivéIllusion.jpg";
+import PrivéIllusionIntense from "../assets/PrivéIllusionIntense.jpg";
+import PrivéModaIsMyIdole from "../assets/PrivéModaIsMyIdole.webp";
+import PrivéPinkRose from "../assets/PrivéPinkRose.jpg";
+import Queen from "../assets/Queen.webp";
+import SenoBeauty from "../assets/SenoBeauty.webp";
+import SerAlAmeera from "../assets/SerAlAmeera.webp";
+import SoPure from "../assets/SoPure.jpg";
+import ValenciaViceVersa from "../assets/ValenciaViceVersa.jpg";
+import MilestoneRivieraInLovesTime from "../assets/MilestoneRivieraInLovesTime.jpeg";
+import SheltonIlang62 from "../assets/SheltonIlang62.jpg";
+import BeforeParadise from "../assets/BeforeParadise.jpg";
+import TeensPinkFlower from "../assets/TeensPinkFlower.jpg";
+import HerBraveryElixir from "../assets/HerBraveryElixir.webp";
+import EyeCandyParfum from "../assets/EyeCandyParfum.jpg";
+import GreatPleasureII from "../assets/GreatPleasureII.jpg";
+import CoffeeAndRoses from "../assets/CoffeeAndRoses.jpg";
+import SaffronMusk from "../assets/SaffronMusk.jpg";
+import EmperTerra from "../assets/EmperTerra.jpg";
+import BrownLeather from "../assets/BrownLeather.webp";
+import RosebombRedIntense from "../assets/RosebombRedIntense.jpg";
+import EmperSelina from "../assets/EmperSelina.webp";
+import PrivéHommeSport from "../assets/PrivéHommeSport.jpg";
+import LarissaAllLove from "../assets/LarissaAllLove.webp";
+import EmperCasual from "../assets/EmperCasual.webp";
 
 export const perfumes = [
-    {
-        "type": "women",
-        "name": "Milestone Cerise Rouge",
-        "image": CeriseRouge,
-        "price": 14
-    },
-    {
-        "type": "unisex",
-        "name": "Milestone Crystal Angel",
-        "image": CrystalAngel,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Al Fares Dala Al Banat",
-        "image": DalaAlBanat,
-        "price": 14
-    },
-    {
-        "type": "men",
-        "name": "Emper Bromusk",
-        "image": EmperBromusk,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Emper High Fashion Niche 053",
-        "image": EmperHighFashionNiche053,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Emper High Fashion Niche 057",
-        "image": EmperHighFashionNiche057,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Emper High Fashion Niche 058",
-        "image": EmperHighFashionNiche058,
-        "price": 14
-    },
-    {
-        "type": "men",
-        "name": "Emper Miramar",
-        "image": EmperMiramar,
-        "price": 14
-    },
-    {
-        "type": "unisex",
-        "name": "Emper Pure Collection Niche 059",
-        "image": EmperPureCollectionNiche059,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Fifth Avenue Of New York",
-        "image": FifthAvenueOfNewYork,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Melina",
-        "image": Melina,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Milestone Madam Moiselle",
-        "image": MilestoneMadamMoiselle,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Milestone Monarch Candy Blast",
-        "image": MilestoneMonarchCandyBlast,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Milestone Rose Blanc",
-        "image": MilestoneRoseBlanc,
-        "price": 14
-    },
-    {
-        "type": "unisex",
-        "name": "Painting's Collection Lord Gordon",
-        "image": PaintingsCollectionLordGordon,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Privé Bordeux",
-        "image": PrivéBordeux,
-        "price": 14
-    },
-    {
-        "type": "men",
-        "name": "Privé Illusion",
-        "image": PrivéIllusion,
-        "price": 14
-    },
-    {
-        "type": "men",
-        "name": "Privé Illusion Intense",
-        "image": PrivéIllusionIntense,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Privé Moda Is My Idole",
-        "image": PrivéModaIsMyIdole,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Privé Pink Rose",
-        "image": PrivéPinkRose,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Milestone Queen",
-        "image": Queen,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Privé Seno Beauty",
-        "image": SenoBeauty,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Al Fares Ser Al Ameera",
-        "image": SerAlAmeera,
-        "price": 14
-    },
-    {
-        "type": "unisex",
-        "name": "So Pure",
-        "image": SoPure,
-        "price": 14
-    },
-    {
-        "type": "women",
-        "name": "Milestone Valencia Vice Versa",
-        "image": ValenciaViceVersa,
-        "price": 14
-    }
-]
+  {
+    type: "women",
+    name: "Milestone Cerise Rouge",
+    image: CeriseRouge,
+    price: 14,
+  },
+  {
+    type: "unisex",
+    name: "Milestone Crystal Angel",
+    image: CrystalAngel,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Al Fares Dala Al Banat",
+    image: DalaAlBanat,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Emper Bromusk",
+    image: EmperBromusk,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Emper High Fashion Niche 053",
+    image: EmperHighFashionNiche053,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Emper High Fashion Niche 057",
+    image: EmperHighFashionNiche057,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Emper High Fashion Niche 058",
+    image: EmperHighFashionNiche058,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Emper Miramar",
+    image: EmperMiramar,
+    price: 14,
+  },
+  {
+    type: "unisex",
+    name: "Emper Pure Collection Niche 059",
+    image: EmperPureCollectionNiche059,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Fifth Avenue Of New York",
+    image: FifthAvenueOfNewYork,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Melina",
+    image: Melina,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Madam Moiselle",
+    image: MilestoneMadamMoiselle,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Monarch Candy Blast",
+    image: MilestoneMonarchCandyBlast,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Rose Blanc",
+    image: MilestoneRoseBlanc,
+    price: 14,
+  },
+  {
+    type: "unisex",
+    name: "Milestone Painting's Collection Lord Gordon",
+    image: PaintingsCollectionLordGordon,
+    price: 14,
+  },
+  {
+    type: "unisex",
+    name: "Milestone Painting's Collection Paonne",
+    image: PaintigsCollectionPaonne,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Privé Bordeux",
+    image: PrivéBordeux,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Privé Illusion",
+    image: PrivéIllusion,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Privé Illusion Intense",
+    image: PrivéIllusionIntense,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Privé Moda Is My Idole",
+    image: PrivéModaIsMyIdole,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Privé Pink Rose",
+    image: PrivéPinkRose,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Queen",
+    image: Queen,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Privé Seno Beauty",
+    image: SenoBeauty,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Al Fares Ser Al Ameera",
+    image: SerAlAmeera,
+    price: 14,
+  },
+  {
+    type: "unisex",
+    name: "So Pure",
+    image: SoPure,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Valencia Vice Versa",
+    image: ValenciaViceVersa,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Riviera In Love's Time",
+    image: MilestoneRivieraInLovesTime,
+    price: 14,
+  },
+  {
+    type: "unisex",
+    name: "Milestone Shelton Ilang 62",
+    image: SheltonIlang62,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Before Paradise",
+    image: BeforeParadise,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Teens Pink Flower",
+    image: TeensPinkFlower,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Her Bravery Elixir",
+    image: HerBraveryElixir,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Privé Eye Candy Parfum",
+    image: EyeCandyParfum,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Milestone Great Pleasure II",
+    image: GreatPleasureII,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Coffee & Roses",
+    image: CoffeeAndRoses,
+    price: 14,
+  },
+  {
+    type: "unisex",
+    name: "Milestone Saffron Musk",
+    image: SaffronMusk,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Emper Terra",
+    image: EmperTerra,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Milestone Brown Leather",
+    image: BrownLeather,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Milestone Rosebomb Red Intense",
+    image: RosebombRedIntense,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Emper Selina",
+    image: EmperSelina,
+    price: 14,
+  },
+  {
+    type: "men",
+    name: "Privé Homme Sport",
+    image: PrivéHommeSport,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Emper Larissa All Love",
+    image: LarissaAllLove,
+    price: 14,
+  },
+  {
+    type: "women",
+    name: "Emper Casual",
+    image: EmperCasual,
+    price: 14,
+  },
+];
